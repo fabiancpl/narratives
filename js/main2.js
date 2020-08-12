016028a633eda55ceb3ecc6060c220901f5f7cf8
Fabian Peña <fabiancpl91@gmail.com>
2020-08-11 22:56:24 -0500

    Improving interactions for mouse actions on nodes

--- js/main2.js
+++ js/main2.js
@@ -23,7 +23,7 @@ var tooltip = d3.select( 'body' ).append( 'div' )
 d3.select( '#background' )
   .on( 'click', function() {
     d3.selectAll( '.scene,.group,.subgroup,.character' )
-      .attr( 'active', false ).style( 'fill-opacity', .05 );
+      .attr( 'active', false ).style( 'fill-opacity', 0 );
     close_panels();
   } );
 
@@ -31,9 +31,9 @@ d3.selectAll( '.scene,.group,.subgroup,.character' )
   .on( 'click', function() {
     var elem = d3.select( this );
     if ( elem.attr( 'active' ) == 'true' ) {
-      elem.attr( 'active', false ).style( 'fill-opacity', .05 );
+      elem.attr( 'active', false ).style( 'fill-opacity', 0 ).style( 'stroke-opacity', .6 );
     } else if ( elem.attr( 'active' ) == 'false' || elem.attr( 'active' ) === null ) {
-      elem.attr( 'active', true ).style( 'fill-opacity', .5 );
+      elem.attr( 'active', true ).style( 'fill-opacity', .9 ).style( 'stroke-opacity', .9 );
     }
     
     if ( elem.attr( 'class' ) === 'scene' ) {
@@ -58,6 +58,13 @@ d3.selectAll( '.scene,.group,.subgroup,.character' )
     var elem = d3.select( this );
     var id = elem.attr( 'id' );
     var text;
+
+    if ( elem.attr( 'active' ) == 'true' ) {
+      
+    } else if ( elem.attr( 'active' ) == 'false' || elem.attr( 'active' ) === null ) {
+      elem.style( 'fill-opacity', .2 ).style( 'stroke-opacity', .6 );
+    }
+
     if ( elem.attr( 'class' ) === 'scene' ) {
       text = scenes.filter( d => d[ 'id' ] === id )[ 0 ][ 'scene' ];
     } else if ( elem.attr( 'class' ) === 'group' ) {
@@ -76,6 +83,14 @@ d3.selectAll( '.scene,.group,.subgroup,.character' )
     return tooltip.style( 'top', ( event.pageY - 15 ) + 'px' ).style( 'left', ( event.pageX + 15 ) + 'px' );
   } )
   .on( 'mouseout', function() {
+    var elem = d3.select( this );
+
+    if ( elem.attr( 'active' ) == 'true' ) {
+      
+    } else if ( elem.attr( 'active' ) == 'false' || elem.attr( 'active' ) === null ) {
+      elem.style( 'fill-opacity', 0 ).style( 'stroke-opacity', .2 );
+    }
+
     return tooltip.style( 'visibility', 'hidden' );
   } );
 
