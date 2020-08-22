8cc41eafbc9afad5b942b3b70c74a7735376ffc7
Luis Mera <luismera92@gmail.com>
2020-08-22 18:57:34 -0500

    Barra superios roja

--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,25 @@
-var hoverMenu = $( '#hidden-menu' ),
-  hoverSpace = $( '#hover-space' );
+var hoverMenu = $( '#hidden-menu' );
+var contentInfo = $( '#info-top-bar' )
 
-hoverSpace
-  .on( 'mousemove', function( event ) {
-    if( 23 - event.clientY < 0 ) {
-      hoverMenu.css( { top: 23 - event.clientY } );
-    } else {
-      hoverMenu.css( { top: 0 } );
-    }
-  } )
-  .on( 'mouseout', function() {
+document.addEventListener('mousemove', mouseUpdate); 
+
+function mouseUpdate (event) { 
+  // console.log(contentInfo.height())
+  // console.log(hoverMenu.height())
+  // console.log({ 
+  //   x : event.pageX, 
+  //   y : event.pageY 
+  // })
+
+  if (event.pageY <= 23 && event.pageY >= 2){
+    hoverMenu.css( { top: 0 } );
+    contentInfo.fadeIn();
+  } else if (event.pageY >= contentInfo.height()) {
     hoverMenu.css( { top: -23 } );
-  } );
+    contentInfo.fadeOut('slow');0
+  } else if (event.pageY < 2) {
+    hoverMenu.css( { top: -23 } );
+    contentInfo.fadeOut('slow');
+  }
+  
+}
